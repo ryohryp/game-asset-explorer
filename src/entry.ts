cc2a4a0478ecd7ac539d9577c1c8e92000ef2547
ryohryp <45325933+ryohryp@users.noreply.github.com>
2026-09-18 01:33:10 +0900

    feat: register Git image diff command

--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,11 @@
 import * as vscode from "vscode";
 import { activate as activateExtension, deactivate as deactivateExtension } from "./extension";
 import { registerGenerateNewAssetPromptCommand } from "./newAssetPromptCommand";
+import { registerGitImageDiffCommand } from "./gitImageDiffCommand";
 
 export function activate(context: vscode.ExtensionContext): void {
   activateExtension(context);
-  context.subscriptions.push(registerGenerateNewAssetPromptCommand());
+  context.subscriptions.push(registerGenerateNewAssetPromptCommand(), registerGitImageDiffCommand());
 }
 
 export function deactivate(): void {
